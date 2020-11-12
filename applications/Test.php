74346645157b9d2ed47b725b71d6c0c4e616f327
Kalle Sommer Nielsen <kalle@php.net>
2020-11-12 07:02:04 +0200

    Update to use Application

---
+++ applications/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Applications;
+
+use Tuxxedo\Application;
+
+class Test extends Application
+{
+	public function run() : void
+	{
+		echo 'Hello World', \PHP_EOL;
+	}
+}
---
+++ public/test/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Applications\Test;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+(new Test)->run();
