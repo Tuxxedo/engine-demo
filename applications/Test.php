483997c6f3f1ee336edcc4b376b1f156f4bf7b58
Kalle Sommer Nielsen <kalle@php.net>
2020-11-13 00:27:36 +0200

    DColt as an author

--- applications/Test.php
+++ applications/Test.php
@@ -3,9 +3,19 @@
 namespace Applications;
 
 use Tuxxedo\Application;
+use Tuxxedo\Di;
+use Tuxxedo\Version;
 
 class Test extends Application
 {
+	public function __construct()
+	{
+		$di = Di::init();
+		$di->register('version', fn() : string => Version::FULL);
+
+		parent::__construct($di);
+	}
+
 	public function run() : void
 	{
 		printf(
--- public/test/index.php
+++ public/test/index.php
@@ -1,13 +1,7 @@
 <?php
 
 use Applications\Test;
-use Tuxxedo\Di;
-use Tuxxedo\Version;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$di = Di::init();
-
-$di->register('version', fn() : string => Version::FULL);
-
-(new Test($di))->run();
+(new Test)->run();
